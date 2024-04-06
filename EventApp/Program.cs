e6ee59785575b5a36326eeefa81dbb1029f01223
Laynexx-ns <argo150808@gmail.com>
2024-04-06 12:37:41 +0300

    lamp event

--- EventApp/Program.cs
+++ EventApp/Program.cs
@@ -10,6 +10,38 @@ namespace EventApp
     {
         static void Main(string[] args)
         {
+            Lamp lamp = new Lamp();
+            lamp.TurnedOn += ReactionOnTurnOn;
+            lamp.TurnOn();
+            
+        }
+        public static void ReactionOnTurnOn(object sender, EventArgs e)
+        {
+            InfoEventArgs info = e as InfoEventArgs;
+            Console.WriteLine(info.Color);
+
+        }
+    }
+    class InfoEventArgs : EventArgs
+    {
+        public string Color
+        {
+            set;
+            get;
+        }
+
+    }
+
+    class Lamp
+    {
+        public event EventHandler TurnedOn;
+
+        public void TurnOn()
+        {
+            if (TurnedOn != null)
+            {
+                TurnedOn(this, new InfoEventArgs() { Color = "Red"});
+            }
         }
     }
 }
